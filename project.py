55c30b918915917adf2b4e7229cd9c9529954226
thisbejim <jimmy_cm3@hotmail.com>
2015-03-13 15:18:11 +1100

    keys2

--- project.py
+++ project.py
@@ -310,6 +310,6 @@ def checkAuth(course_id):
         return False
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
+    
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
